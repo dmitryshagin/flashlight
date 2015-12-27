b8bb059636454b7d0aa1f52a7291fce9475419ed
Dmitry Shagin <dmitry.shagin@gmail.com>
2015-12-27 17:43:36 +0200

    comments translated for pwm controller

--- driver/pwm_control_tiny10/main.c
+++ driver/pwm_control_tiny10/main.c
@@ -7,39 +7,34 @@
 #define LED_100_PERCENT 0xFF
 #define ADC_LIMIT 0xFF>>1
 
-volatile uint8_t is_sleeping;
-volatile uint8_t current_mode;
-volatile uint8_t to_apply;
-
-// PB0 - ADC in; PB1 - PWM out; PB2 - INTERRUPUT IN
+volatile uint8_t need_to_sleep, current_mode, to_apply;
 
 ISR(INT0_vect){
 	cli();
-	ADCSRA = 0; //АЦП вырубим
-	EIMSK = 0; //вырубим INT0 (дабы избежать дребезга)
-	if (PINB & (1<<PB2)){ //высокий уровень PB2 - значит у нас пропало питание
-		EICRA = 0; //перенастроим прерывание по INT0 на низкий уровень для определения наличия питания
-		is_sleeping = 1; //флажок, по которому главный цикл погонит контроллер в спячку
-		current_mode = 1-current_mode; //после просыпания нужен будет другой режим, выберем его сразу
-	}else{ //низкий уровень, значит питание вернулось
-		EICRA = (1<<ISC00)|(1<<ISC01);//переключим прерывание на высокий уровень		
-		is_sleeping = 0; //поставим флажок, что пора просыпаться
+	ADCSRA = 0;		//turning off ADC
+	EIMSK = 0;		//turning off INT0
+	if (PINB & (1<<PB2)){	//high level => we've got power loss! let's go to sleep
+		EICRA = 0;			//INT0 will now trigger on low level (only possible option in PWD down)
+		need_to_sleep = 1;
+		current_mode = 1-current_mode;
+	}else{			//low level => we've got power back :) lets' wake up
+		EICRA = (1<<ISC00)|(1<<ISC01);	//INT0 will now trigger on rising edge to detect power loss
+		need_to_sleep = 0;
 	}
-	WDTCSR = (1<<WDIE) | (1<<WDP0) | (1<<WDP1); //поставим WDT на 0.1с, чтобы подождать стабилизации питания 
-	
+	WDTCSR = (1<<WDIE) | (1<<WDP0) | (1<<WDP1);	//will apply changes in 0.1 second with WDT
 }
 
 ISR(WDT_vect){
-	cli(); // прошло время антидребезга - врубим прерывания обратно, применим изменения
-	if(!is_sleeping){
-		to_apply = 1;//надо применить новый режим
+	cli();
+	if(!need_to_sleep){
+		to_apply = 1;
 	}	
-	WDTCSR = 0; //вырубим WDT, он больше пока не нужен
-	EIMSK = (1<<INT0);//так как WDT только на антидребезг работает, то выключим его и включим обратно INT0
+	WDTCSR = 0;			//don't need WTD - let's turn on INT0 back on
+	EIMSK = (1<<INT0);
 }
 
 ISR(ADC_vect){
-	ADCSRA |= 1<<ADSC;		// Start Conversion
+	ADCSRA |= 1<<ADSC;	//start conversion
 	if(ADCL<ADC_LIMIT){
 		OCR0B = LED_LOW_VOLTAGE;
 	}
@@ -47,24 +42,24 @@ ISR(ADC_vect){
 
 
 int main(void){
+	// PB0 - ADC in; PB1 - PWM out; PB2 - INTERRUPUT IN, PB3 - unused;
+	DDRB  = (0<<DDB0) | (1<<DDB1) | (0<<DDB2) | (0<<DDB3);
 
-	DDRB  = (0<<DDB0) | (1<<DDB1) | (0<<DDB2) | (0<<DDB3); //первая нога - выход? остальные - вход
-
-	EICRA = (1<<ISC00)|(1<<ISC01);//разрешаем int0 по смене сигнала (для определения выключения питания)
+	EICRA = (1<<ISC00) | (1<<ISC01);				//INT0 by rising edge to detect power loss
 	EIMSK = (1<<INT0);
 
-	TCCR0A = (1 << COM0B1) | (1 << WGM00);  // phase correct PWM mode
-    TCCR0B = (1 << CS01);   // clock source = CLK/8, start PWM
-    OCR0B = LED_100_PERCENT;
+	TCCR0A = (1 << COM0B1) | (1 << WGM00);  		// PWM mode
+	TCCR0B = (1 << CS01);							// clock source = CLK/8, start PWM
+	OCR0B = LED_100_PERCENT;
 
-    ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADIE);             // enable ADC on 1mhz/16 with interrupt
-    ADCSRA |= (1<<ADSC); //start conversion
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADIE);	// enable ADC on 1mhz/16 with interrupt
+	ADCSRA |= (1<<ADSC);							//start conversion
 
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 
 	while(1){
 		sei();
-		if(is_sleeping){
+		if(need_to_sleep){
 			sleep_mode();
 		}
 		if(to_apply==1){
